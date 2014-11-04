e9e9b031398247cb77f513329921c401ca3792c9
INOUE Takuya <inouetakuya5@gmail.com>
2014-11-05 00:02:44 +0900

    コードを整形して適宜コメントを付けた

--- samples/chapter4/guthub/app/scripts/services/services.js
+++ samples/chapter4/guthub/app/scripts/services/services.js
@@ -1,35 +1,41 @@
 ﻿'use strict';
 
 var services = angular.module('guthub.services',
-    ['ngResource']);
+  ['ngResource']);
 
 services.factory('Recipe', ['$resource',
-    function($resource) {
-  return $resource('/recipes/:id', {id: '@id'});
-}]);
+  function ($resource) {
+    return $resource('/recipes/:id', {id: '@id'});
+  }]);
 
 services.factory('MultiRecipeLoader', ['Recipe', '$q',
-    function(Recipe, $q) {
-  return function() {
-    var delay = $q.defer();
-    Recipe.query(function(recipes) {
-      delay.resolve(recipes);
-    }, function() {
-      delay.reject('レシピの取得に失敗しました');
-    });
-    return delay.promise;
-  };
-}]);
+  function (Recipe, $q) {
+    return function () {
+      var delay = $q.defer();
+      Recipe.query(function (recipes) {
+        delay.resolve(recipes);
+      }, function () {
+        delay.reject('レシピの取得に失敗しました');
+      });
+      return delay.promise;
+    };
+  }]);
 
 services.factory('RecipeLoader', ['Recipe', '$route', '$q',
-    function(Recipe, $route, $q) {
-  return function() {
-    var delay = $q.defer();
-    Recipe.get({id: $route.current.params.recipeId}, function(recipe) {
-      delay.resolve(recipe);
-    }, function() {
-      delay.reject('レシピの取得に失敗しました: '  + $route.current.params.recipeId);
-    });
-    return delay.promise;
-  };
-}]);
+  function (Recipe, $route, $q) {
+    return function () {
+      // $q は promise オブジェクト
+      var delay = $q.defer();
+
+      Recipe.get({id: $route.current.params.recipeId}, function (recipe) {
+
+        // サーバーがレスポンスを返したら promise オブジェクトに実際のオブジェクトをセットする
+        delay.resolve(recipe); // 約束が達成され、成功時のコールバックが実行される
+
+      }, function () {
+        // rejext で約束が破棄され、エラーハンドラが実行される
+        delay.reject('レシピの取得に失敗しました: ' + $route.current.params.recipeId);
+      });
+      return delay.promise;
+    };
+  }]);
